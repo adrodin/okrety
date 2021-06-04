69260353ddcb519ab91bc4b64b6e951571a3152b
adrodin <adrian.dochnal@student.pk.edu.pl>
2021-06-04 19:02:26 +0200

    add random ships generation function

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
                 if exit_button.is_mouse_over(pygame.mouse.get_pos()):
                     run = False
                 if reset_button.is_mouse_over(pygame.mouse.get_pos()):
-                    print(board2.check_ships_position())
+                    print(board2.automatic_ships_generator())
                     pass
         pygame.display.update()
 
--- src/board.py
+++ src/board.py
@@ -1,9 +1,12 @@
 import pygame
+import random
 from enum import Enum
 from src.field import Field,Field_states,Players_type
 
 class Board:
     def __init__(self,x_start,y_start,window,player_type):
+        self._x_start = x_start
+        self._y_start = y_start
         self._state = Board_state.PREPARE
         self._player_type = player_type
         self._window = window
@@ -32,7 +35,6 @@ class Board:
         }
         for i in range(10):
             for j in range(10):
-                print(f'xd {i} {j}')
                 if i_was_here[i][j] == 'no':
                     i_was_here[i][j] = 'yes'
                     if self._fields[i][j].get_field_state() == Field_states.SHIP:
@@ -42,7 +44,6 @@ class Board:
                         #statek w rzedzie
                         for k in range(1,5):
                             if j+k < 10:
-                                print(k)
                                 if i_was_here[i][j+k] == 'yes' and self._fields[i][j+k].get_field_state() == Field_states.SHIP:
                                     return False
                                 if self._fields[i][j+k].get_field_state() == Field_states.SHIP and j != 9:
@@ -101,6 +102,83 @@ class Board:
 
     def check_win(self):
         return len(self._ships == 0)
+
+    def automatic_ships_generator(self):
+        ok = False
+        it = 0
+
+        self._fields = [[Field(self._x_start + j * 40, self._y_start + i * 40, self._window, self._player_type) for j in range(10)] for i in range(10)]
+        self._ships = []
+        #---4x1
+        x = random.randrange(7)
+        y = random.randrange(7)
+        z = random.randrange(7)
+        self.add_ship(x,y,z,4)
+        #3x2
+        for _ in range(2):
+            success = False
+            while not success:
+                x = random.randrange(8)
+                y = random.randrange(8)
+                z = random.randrange(2)
+                success = self.can_add_ship(x,y,z,3)
+                if success:
+                     self.add_ship(x,y,z,3)
+        #2x3
+        for _ in range(3):
+            success = False
+            while not success:
+                x = random.randrange(9)
+                y = random.randrange(9)
+                z = random.randrange(2)
+                success = self.can_add_ship(x,y,z,2)
+                if success:
+                    self.add_ship(x,y,z,2)
+        #1x4
+        for _ in range(4):
+            success = False
+            while not success:
+                x = random.randrange(10)
+                y = random.randrange(10)
+                z = random.randrange(2)
+                success = self.can_add_ship(x, y, z, 1)
+                if success:
+                    self.add_ship(x, y, z, 1)
+        return True
+
+    def can_add_ship(self,x,y,z,size):
+        if z == 0:
+            for i in range(-1, size+1):
+                if x + i >= 0 and x + i < 10:
+                    if self._fields[x + i][y].get_field_state() == Field_states.SHIP:
+                        return False
+                    if y+1 < 10:
+                        if self._fields[x + i ][y+1].get_field_state() == Field_states.SHIP:
+                            return False
+                    if y - 1 >= 0:
+                        if self._fields[x + i ][y-1].get_field_state() == Field_states.SHIP:
+                            return False
+
+        else:
+            for i in range(-1, size+1):
+                if y + i >= 0 and y + i < 10:
+                    if self._fields[x][y + i].get_field_state() == Field_states.SHIP:
+                        return False
+                    if x+1< 10:
+                        if self._fields[x+1][y + i ].get_field_state() == Field_states.SHIP:
+                            return False
+                    if x -1 >= 0:
+                        if self._fields[x-1][y + i ].get_field_state() == Field_states.SHIP:
+                            return False
+        return True
+
+    def add_ship(self,x,y,z,size):
+        if z == 0:
+            for i in range(size):
+                self._fields[x + i][y].draw_ship_segment()
+        else:
+            for i in range(size):
+                self._fields[x][y + i].draw_ship_segment()
 
 class Board_state(Enum):
     PREPARE = 0
