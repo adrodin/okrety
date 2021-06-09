83263a3d81109b05ec3634db88db45aa0220704c
adrodin <adrian.dochnal@student.pk.edu.pl>
2021-06-09 17:58:40 +0200

    add button docs

--- src/button.py
+++ src/button.py
@@ -6,6 +6,14 @@ class Button:
     def __init__(self,x,y,width,height,color=BUTTON_COLOR,text='',text_color=BUTTON_TEXT_COLOR ):
         """
         Inicjalizacja przycisku
+
+        :param x: pozycja x przycisku
+        :param y: pozycja y przycisku
+        :param width: szerokość przycisku
+        :param height: wysokość przycisku
+        :param color: kolor przycisku
+        :param text: teskt przycisku
+        :param text_color: kolor tekstu w przycisku
         """
         self._x = x
         self._y = y
@@ -19,6 +27,8 @@ class Button:
     def is_mouse_over(self,mouse_position):
         """
         Sprawdzenie pozycji myszy względem przycisku
+        :param mouse_position: pozycja myszki zapisana jako tuple (x,y)
+        :return: True jeżeli myszka jest nad przyciskie, False w przeciwnym wypadku
         """
         is_mouse_inside_x = self._x < mouse_position[0] < self._x + self._width
         is_mouse_inside_y = self._y < mouse_position[1] < self._y + self._height
@@ -27,6 +37,7 @@ class Button:
     def draw(self, window):
         """
         Narysowanie przycisku
+        :param window: okno w którym ryoswany będzie przycisk
         """
         pygame.draw.rect(window, self._color, (self._x, self._y, self._width, self._height), 0)
         if self._text != '':
@@ -38,6 +49,8 @@ class Button:
     def change_color(self,color,window):
         """
         Zmiana koloru
+        :param color: nowy kolor przycisku
+        :param window: okno w, w którym rysowany będzie przycisk
         """
         self._color = color
         self.draw(window)
@@ -45,6 +58,8 @@ class Button:
     def change_text(self,new_text,window):
         """
         Zmiana tekstu
+        :param new_text: nowy tekst wyświetlany w przycisku
+        :param window: okno w, w którym rysowany będzie przycisk
         """
         self._text = new_text
         self.draw(window)
@@ -52,6 +67,9 @@ class Button:
     def change_text_with_new_color(self,new_text,new_color,window):
         """
         Zmiana tekstu wraz z kolorem
+        :param new_text: nowy tekst wyświetlany w przycisku
+        :param new_color: nowy kolor tekstu wyświetlanego w przycisku
+        :param window: okno w, w którym rysowany będzie przycisk
         """
         self._text = new_text
         self._text_color = new_color
